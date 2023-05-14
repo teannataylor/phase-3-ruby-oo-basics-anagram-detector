a5389c2aa86528e6d9346fe7ac757e91acd07048
Teanna <teannamtaylor95@gmail.com>
2023-05-14 14:42:36 -0700

    commit tree 250505cfa2a389c3eed70a88b38aea06c5d1efb2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+
+    def match(list)
+        list.filter do |thisword|
+            thisword.chars.sort == @word.chars.sort
+        end
+    end
+
+end
